ab71ae26f5246285d09f43a527f47fefac169cbc
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-06-28 21:49:41 +0600

    admin

--- index.js
+++ index.js
@@ -51,6 +51,39 @@ async function run(){
         res.send(products);
        });
 
+       app.get('/user', verifyJWT, async(req, res) =>{
+        const users = await userCollection.find().toArray();
+        res.send(users);
+       })
+
+      //  admin
+      
+      app.get('/admin/:email', async(req, res) =>{
+        const email = req.params.email; 
+        const user = await userCollection.findOne({email:email});
+        const isAdmin = user.role === 'admin';
+        res.send({admin: isAdmin})
+      })
+
+      app.put('/user/admin/:email', verifyJWT, async(req, res) =>{
+        const email = req.params.email;
+        const requester = req.decoded.email;
+        const requesterAccount = await userCollection.findOne({email: requester});
+        if(requesterAccount.role === 'admin'){
+          const filter = {email: email};
+          const updateDoc = {
+               $set:{role:'admin'},
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result); 
+        }
+        else{
+          res.status(403).send({message:'forbidden'});
+        }
+       
+      })
+
+      // user
        app.put('/user/:email', async(req, res) =>{
          const email = req.params.email;
          console.log(email);
@@ -112,12 +145,12 @@ async function run(){
       });
 
       // admin
-      app.get("/admin/:email", async (req, res) => {
-        const email = req.params.email;
-        const user = await userCollection.findOne({ email: email });
-        const isAdmin = user.role === "admin";
-        res.send({ admin: isAdmin });
-      });
+      // app.get("/admin/:email", async (req, res) => {
+      //   const email = req.params.email;
+      //   const user = await userCollection.findOne({ email: email });
+      //   const isAdmin = user.role === "admin";
+      //   res.send({ admin: isAdmin });
+      // });
  
     }
     finally{
