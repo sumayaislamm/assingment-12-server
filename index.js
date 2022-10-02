addbab3e3fd0af9d6f1a01136147cb4d957753c4
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-10-03 01:08:47 +0600

    homepage done

--- index.js
+++ index.js
@@ -157,15 +157,7 @@ async function run(){
          
        })
 
-      
-
-      // admin
-      // app.get("/admin/:email", async (req, res) => {
-      //   const email = req.params.email;
-      //   const user = await userCollection.findOne({ email: email });
-      //   const isAdmin = user.role === "admin";
-      //   res.send({ admin: isAdmin });
-      // });
+    
  
     }
     finally{
